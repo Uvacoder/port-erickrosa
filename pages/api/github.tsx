c6dc50ed59bd7d320b61258c2a86b2b7a9412cda
uvacoder <uva22940coder@gmail.com>
2022-11-27 00:13:58 -0500

    Update github.tsx

--- pages/api/github.tsx
+++ pages/api/github.tsx
@@ -1,5 +1,5 @@
 export default async (req, res) => {
-    const url = "https://api.github.com/users/RosaErick/repos?per_page=100"
+    const url = "https://api.github.com/users/uvacoder/repos?per_page=100"
     const response = await fetch(url)
     const json = await response.json()
 
